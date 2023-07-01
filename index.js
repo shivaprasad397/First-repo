421bf29da8f5dae138c326d02465dd2da55f2495
shivaprasad397 <shivaprasadkurva397@gmail.com>
2023-07-01 14:10:27 +0530

    initialcommit

---
+++ index.js
@@ -0,0 +1,64 @@
+
+// var myne = {
+//     name:"shiva",
+//     age:21,
+//     height:1.6,
+//     weight:48,
+
+// };
+// console.log(myne.age)
+// let a=7;
+// let b=8;
+// let c= ++a * --b;
+// let d= --a * ++b;
+// let e= --a * --a;
+// let f= ++b * ++b
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// let no1=100;
+// console.log(no1++);
+// console.log(no1++);
+// let number1=5;
+// let number2=5;
+// console.log(number2!==number1);
+// let age=prompt("Enter your age:-");
+// let status=(age>18)?"you are elgible for voting":"Not elgible";
+// console.log(status);//here it is depricsted so striked on status
+// let no1;
+// no1=prompt("enter no1:");
+// let no2;
+// no1=prompt("enter no2:");
+// let no3;
+// no1=prompt("enter no3:");
+// if(no1>=no2 && no1>=no3){
+//     console.log("number one is greatest:");
+// }
+// else if(no2>=1 && no2>=no3){
+//     console.log("number two is greatest:");
+// }
+// else{
+//     console.log("number three is greatest:");
+// }
+
+
+let marks;
+marks=prompt("enter your marks")
+if(marks>=90 && marks<=100){
+    console.log("Grade is A");
+}
+else if(marks>=80 && marks<=90){
+    console.log("Grade is B");
+}
+else if(marks>=70 && marks<=80){
+    console.log("Grade is C");
+}
+else if(marks>=60 && marks<=70){
+    console.log("Grade is D");
+} else{
+  
+console.log("I think u failed");
+    
+}
+
